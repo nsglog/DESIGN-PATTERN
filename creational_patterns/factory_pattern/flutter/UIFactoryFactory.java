084afa6b418c8e2f67a12b090dd6ba9b1db44245
unzipnav <unzip.nav@gmail.com>
2022-12-27 03:11:51 +0530

    factory pattern

--- creational_patterns/factory_pattern/flutter/UIFactoryFactory.java
+++ creational_patterns/factory_pattern/flutter/UIFactoryFactory.java
@@ -1,3 +1,5 @@
+//this is the implementation of practical factory
+
 package flutter.flutter;
 
 import flutter.flutter.SupportedPlatforms;
@@ -7,6 +9,7 @@ public class UIFactoryFactory {
     
     public static UIFactory getUIFactoryForPlatform (SupportedPlatforms platform) {
         
+        // this code structure below will return the desired object from the factory of various available classes
         switch (platform)    {
             
             case ANDROID : return new AndroidUIFactory();
